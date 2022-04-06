41ada7c47a1ece590f19f152c339adcba81e61f4
Leo Rigotto <leorigotto@me.com>
2022-04-06 11:18:51 -0300

    proteçao de dados do servidor

--- src/connection.ts
+++ src/connection.ts
@@ -1,12 +1,16 @@
 import knex from "knex"
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const connection = knex({
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "techshare",
-    password: "techshare123",
-    database: "technical_share",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
     multipleStatements: true,
   },
 })
