0ec444ca2362019c85377d240995f7838fe81e91
Leo Rigotto <leorigotto@me.com>
2022-04-06 14:48:59 -0300

    ajustes de servidor

--- src/app.ts
+++ src/app.ts
@@ -16,32 +16,4 @@ const server = app.listen(process.env.PORT || 3003, () => {
   }
 })
 
-app.get("/users", async (req, res) => {
-  try {
-    const result = await connection.raw('SELECT * FROM Users;')
-    res.send(result[0])
-  } catch (error: any) {
-    res.send(error.message)
-  }
-})
-
-app.post("/users", async (req: Request, res: Response) => {
-  try {
-    await connection.raw(`
-        INSERT INTO Users
-           (name, email, password, photo, bio, links, role)
-        VALUES (
-           "${req.body.name}",
-           "${req.body.email}",
-           "${req.body.password}",
-           "${req.body.photo}",
-           "${req.body.bio}",
-           "${req.body.links}",
-           "${req.body.role}"
-); `)
-    res.status(201).send("Success!")
-  } catch (error: any) {
-    console.log(error.message)
-    res.status(500).send("An unexpected error occurred")
-  }
-})
+export default app
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express"
+import app from "./app"
+import connection from "./connection"
+
+app.get("/users", async (req, res) => {
+  try {
+    const result = await connection.raw('SELECT * FROM Users;')
+    res.send(result[0])
+  } catch (error: any) {
+    res.send(error.message)
+  }
+})
+
+app.post("/users", async (req: Request, res: Response) => {
+  try {
+    await connection.raw(`
+        INSERT INTO Users
+           (name, email, password, photo, bio, links, role)
+        VALUES (
+           "${req.body.name}",
+           "${req.body.email}",
+           "${req.body.password}",
+           "${req.body.photo}",
+           "${req.body.bio}",
+           "${req.body.links}",
+           "${req.body.role}"
+); `)
+    res.status(201).send("Success!")
+  } catch (error: any) {
+    console.log(error.message)
+    res.status(500).send("An unexpected error occurred")
+  }
+})
